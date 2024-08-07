24885815166432368018d7a716f99fc0c325132f
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-08-07 12:19:28 +0100

    Change reddit client stuff

--- bot/functions/main.py
+++ bot/functions/main.py
@@ -4,16 +4,18 @@ from functions.get_meme import get_subreddit, get_submissions
 
 from functions.handle_json import read_json
 
+from bot_token import reddit_client
+
 def start_reddit_instance(subreddit_name=None, guild_id=None):
 
     if (subreddit_name == None):
         subreddit_name = get_subreddit(guild_id=guild_id)
 
     reddit = praw.Reddit(
-        client_id="2Gl-Fy1aB7WsFt5Ltyp9zw",
-        client_secret="cyuXfIHCxwtrpOSA3-p5dZHNOUhs_A",
-        redirect_uri="https://www.reddit.com/r/Catmemes",
-        user_agent="cat memes script",
+        client_id=reddit_client[0],
+        client_secret=reddit_client[1],
+        redirect_uri=reddit_client[2],
+        user_agent=reddit_client[3],
     )
 
     subreddit = reddit.subreddit(subreddit_name)
